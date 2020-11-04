c881687165681a4895fdaa0e9440f20beeae2582
Yunjae-Na <yunjaena@gmail.com>
2020-11-04 21:39:29 +0900

    Add project
    Add project

---
+++ KoreatechGraduateManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KoreatechGraduateManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
