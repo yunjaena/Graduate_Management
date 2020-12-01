a98ce65510ee785fe1a2536cdb370c82dd4679fd
Yunjae-Na <yunjaena@gmail.com>
2020-12-02 06:35:00 +0900

    Add subject entity column
    Add subject entity column

--- KoreatechGraduateManagement/Models/Subject.cs
+++ KoreatechGraduateManagement/Models/Subject.cs
@@ -7,6 +7,20 @@ namespace KoreatechGraduateManagement.Models
 {
     public class Subject
     {
+        public int Id { get; set; }
 
+        public int Semester { get; set; }
+
+        public String SubjectCode { get; set; }
+
+        public String ClassNumber { get; set; }
+
+        public String SubjectType { get; set; }
+
+        public String Target { get; set; }
+
+        public String ClassTime { get; set; }
+
+        public int Credit { get; set; }
     }
 }
